c9cde2cf0ae1552b2440311f8bba01d003c41f69
lm1802@gmail.com <lm1802@gmail.com>
2023-05-16 14:48:47 -0400

    changed text

--- helloworld_1/helloworld_1.cs
+++ helloworld_1/helloworld_1.cs
@@ -68,7 +68,7 @@ namespace helloworld_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation(text: "Hello World");
+			engine.GenerateInformation(text: "Hello World this is god");
 	
 		}
 	}
